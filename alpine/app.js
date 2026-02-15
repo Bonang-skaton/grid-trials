cc34f7ca86b91464c05ea94890ab499b06ec0235
Bonang Skaton <bonang1skaton@gmail.com>
2026-02-15 23:30:19 +0700

    hgn

--- alpine/app.js
+++ alpine/app.js
@@ -111,6 +111,7 @@ const formatMessage = (obj) => {
    Nama: ${obj.name}
    No Hp: ${obj.phone}
    Alamat: ${obj.address}
+   
    Pesanan Online
  ${JSON.parse(obj.items).map((item) => `${item.name} (${item.quantity} x ${rupiah(item.total)}) \n`)}
  TOTAL: ${rupiah(obj.total)}
